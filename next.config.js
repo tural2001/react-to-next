de5488be7e8c2c2421c9d25d62b18db010276059
tural2001 <you@example.com>
2023-08-11 14:10:02 +0400

    new

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 module.exports = {
   webpack: (config, options) => {
     if (!options.dev) {
-      config.devtool = options.isServer ? false : 'your-custom-devtool';
+      config.devtool = options.isServer ? false : 'source-map';
     }
     return config;
   },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'http',
-  //       hostname: 'localhost:3000',
-  //       port: '',
-  //       pathname: '/my-bucket/**',
-  //     },
-  //   ],
-  // },
 };
